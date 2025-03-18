198980dfb3a8e446d1bd90a57a1d7f011dbcae3e
9244-cyber <dym8236@163.com>
2025-03-18 18:05:45 +0800

    gx

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
-APP = ['zzfw_New.py']
-DATA_FILES = []
+APP = ['zzfw_mac.py']       # 入口文件
+DATA_FILES = []         # 其他数据文件（如图片、配置文件）
 OPTIONS = {
     'argv_emulation': True,
-    'packages': ['flask','requests','gmssl','base64','re','json','urllib3','datetime']
+    'plist': {
+        'CFBundleName': 'zzfw_mac',  # 应用名称
+        'CFBundleVersion': '1.0',
+        'CFBundleShortVersionString': '1.0',
+    }
 }
 
 setup(
