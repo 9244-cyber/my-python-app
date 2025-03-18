c69bab3d3665e1cb438ddc103f6a2544e87e7123
9244-cyber <dym8236@163.com>
2025-03-18 17:55:48 +0800

    gx

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+APP = ['zzfw_New.py']
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': True,
+    'packages': ['flask','requests','gmssl','base64','re','json','urllib3','datetime']
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
